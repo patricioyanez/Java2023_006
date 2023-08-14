72c43df8b1ac0ebd43d80b557474d5728296dd0d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-14 13:25:30 -0400

    Clase 1

---
+++ PrimerEjemplo/src/Pizza.java
@@ -0,0 +1,29 @@
+
+public class Pizza {
+    // atributos
+    private String nombre;
+    private String tamano;
+    private String masa;
+    private int precio;
+    // constructor
+    public Pizza()
+    {
+        nombre = "";
+        tamano = "personal";
+        masa = "";
+        precio = 0;
+    }
+    public Pizza(String nombre, String tamano, String masa, int precio)
+    {
+        this.nombre = nombre;
+        this.tamano = tamano;
+        this.masa = masa;
+        this.precio = precio;
+    }
+    
+    // getter and setter / accesador y mutador
+    
+    
+    // métodos customer
+    
+}
---
+++ PrimerEjemplo/src/Principal.java
@@ -0,0 +1,15 @@
+// comentario simple
+/*
+ comentario de más de una línea
+*/
+public class Principal {
+    // método main ( entry point)
+    public static void main(String args[])
+    {  // ppt 1.2.1
+        System.out.println("Hola Mundo!!!");
+        // crear objeto
+        Pizza pizza1 = new Pizza();
+        Pizza pizza2 = new Pizza("Española", "Mediana", "Delgada", 8000);
+    }
+    
+}
