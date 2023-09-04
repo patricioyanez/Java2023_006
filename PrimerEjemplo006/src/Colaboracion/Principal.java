fe4ea86d9b0b713c629a9c0d393bd792763405af
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-04 12:00:01 -0300

    Clase 4

--- PrimerEjemplo006/src/Colaboracion/Principal.java
+++ PrimerEjemplo006/src/Colaboracion/Principal.java
@@ -1,6 +1,8 @@
 
 package Colaboracion;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         
@@ -27,5 +29,23 @@ public class Principal {
         // Crear otra canción y reproducirla.
         // solicitar los datos por la consola. 
         // todos los datos son obligatorios.
+        
+        Cancion cancion2 = new Cancion();
+        
+        Scanner leerTeclado = new Scanner(System.in);
+        System.out.print("Nombre    : ");
+        cancion2.setNombre(leerTeclado.next());
+        System.out.print("Banda     : ");
+        cancion2.setArtista(leerTeclado.next());
+        System.out.print("Album     : ");
+        cancion2.setAlbum(leerTeclado.next());
+        System.out.print("Género    : ");
+        cancion2.setGenero(leerTeclado.next());
+        System.out.print("Duración  : ");
+        cancion2.setDuracion(leerTeclado.nextInt());
+        
+        spotify.setCancion(cancion2);
+        spotify.setSegundosReproducidos(50);
+        System.out.println(spotify.imprimir());
     }
 }
