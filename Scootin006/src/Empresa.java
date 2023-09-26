fb7bc186502a8ba141bbfe2405e8208858b7c320
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-26 12:11:58 -0300

    update

--- Scootin006/src/Empresa.java
+++ Scootin006/src/Empresa.java
@@ -11,4 +11,36 @@ public class Empresa {
     {
         empleados = new ArrayList<Empleado>();
     }
+    // continuar con ejercicio ppt
+    public boolean agregarEmpleado(Empleado empleado)
+    {
+        // evitar que se agregue 2 veces el mismo rut
+        return empleados.add(empleado);
+    }
+    
+    public boolean buscarEmpleado(String rut)
+    {
+        //recorrer la colección
+        for (Empleado aux : empleados) {
+            if(aux.getRut().equals(rut))
+                return true;
+        }
+        return false;
+    }
+    public void listarEmpleados()
+    {
+        System.out.println("**** Listado de empleados *****");
+        for (Empleado empleado : empleados) {
+            System.out.println(empleado.imprimir());
+        }
+    }
+    public boolean eliminarEmpleado(String rut)
+    {
+        //recorrer la colección para eliminar
+        for (Empleado aux : empleados) {
+            if(aux.getRut().equals(rut))
+                return empleados.remove(aux);
+        }
+        return false;
+    }
 }
