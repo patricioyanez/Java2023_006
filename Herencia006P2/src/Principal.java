f4dcca83661b49f5dcc734c85b8257de13b1e2b4
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-10 12:38:50 -0300

    Ejercicio

--- Herencia006P2/src/Principal.java
+++ Herencia006P2/src/Principal.java
@@ -2,6 +2,8 @@
 public class Principal {
     public static void main(String[] args) {
         Vendedor v = new Vendedor();
-        
+        // Realizar actividad 
+        // 2.2.2 Actividad Formativa Herencia
+        // disponible en el AVA
     }
 }
