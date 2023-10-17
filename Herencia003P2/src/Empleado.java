3ca19bf84e3bcf393425fd2e21bbeec4be809068
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-17 10:24:36 -0300

    update

--- Herencia003P2/src/Empleado.java
+++
@@ -1,9 +0,0 @@
-
-public class Empleado extends Persona{
-    String sucursal;
-    
-    public Empleado()
-    {
-        System.out.println("2.- Es un empleado");
-    }
-}
--- Herencia003P2/src/Principal.java
+++
@@ -1,7 +0,0 @@
-
-public class Principal {
-    public static void main(String[] args) {
-        Vendedor v = new Vendedor();
-        // Leer ppt 2.3.1
-    }
-}
