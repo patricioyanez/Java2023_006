30e16261ab1b393209b890c4b771124dd7a6d670
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-30 11:52:03 -0300

    Update FrmMarca.java

--- EjemploInterfazGrafica006/src/Vista/FrmMarca.java
+++ EjemploInterfazGrafica006/src/Vista/FrmMarca.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vista;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author patri
- */
+
 public class FrmMarca extends javax.swing.JFrame {
 
-    /**
-     * Creates new form FrmMarca
-     */
+
     public FrmMarca() {
         initComponents();
     }
