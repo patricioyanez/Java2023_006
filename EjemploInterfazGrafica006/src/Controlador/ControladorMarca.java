c8a19d44965d1af847d09e9340627156f425c251
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-06 11:48:05 -0300

    Controlador MVC

---
+++ EjemploInterfazGrafica006/src/Controlador/ControladorMarca.java
@@ -0,0 +1,31 @@
+
+package Controlador;
+
+import Modelo.Marca;
+import java.util.ArrayList;
+
+
+public class ControladorMarca {
+    public boolean agregar(Marca marca)
+    {
+        return true;
+    }
+    public boolean actualizar(Marca marca)
+    {
+        return true;
+    }
+    public boolean eliminar(int id)
+    {
+        return true;
+    }
+    
+    public ArrayList<Marca> listarTodos()
+    {
+        return null;
+    }
+    
+    public Marca buscarPorId(int id)
+    {
+        return null;
+    }
+}
