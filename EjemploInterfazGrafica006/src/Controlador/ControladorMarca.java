b0f119d256261f483c82777718f8884d4b589f32
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-07 11:16:12 -0300

    Update ControladorMarca.java

--- EjemploInterfazGrafica006/src/Controlador/ControladorMarca.java
+++ EjemploInterfazGrafica006/src/Controlador/ControladorMarca.java
@@ -103,7 +103,30 @@ public class ControladorMarca {
     }
     public ArrayList<Marca> listarTodos()
     {
-        return null;
+        ArrayList<Marca> listado = new ArrayList<Marca>();
+        try{
+            Conexion con = new Conexion();
+            Connection cx = con.obtenerConexion();
+            String sql = "SELECT ID, NOMBRE, HABILITADO FROM MARCA";
+            PreparedStatement st;
+            st = cx.prepareStatement(sql);
+            
+            ResultSet rs = st.executeQuery();
+            
+            while(rs.next())
+            {
+                Marca marca = new Marca();
+                marca.setId(rs.getInt("ID"));
+                marca.setNombre(rs.getString("NOMBRE"));
+                marca.setHabilitado(rs.getBoolean("HABILITADO"));
+                listado.add(marca);
+            }            
+            st.close();
+            cx.close();
+        } catch(SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return listado;
     }
     
 }
